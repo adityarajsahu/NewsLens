5a8ac55ebdcf59e33d0bec3165f9cd31ef02dd9d
Aditya <adityaraj2019.sahu@gmail.com>
2023-04-19 15:37:09 +0530

    utils.py for web scrapping

--- NewsScrapper/views.py
+++ NewsScrapper/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from .utils import *
 
 # Create your views here.
+
 def homepage(request):
     return render(request, 'home.html')
+
