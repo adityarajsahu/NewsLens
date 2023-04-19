422cfbb41e790b1cf80e5c426072329c3d2aac4e
Sanimesh96 <sanimesh96@gmail.com>
2023-04-19 16:17:33 +0530

    Templates and Model Added

--- NewsScrapper/models.py
+++ NewsScrapper/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+language_choices = (
+    ('Turkish','Turkish'),
+    ('English', 'English'),
+    ('Arabic', 'Arabic'),
+    ('Farsi', 'Farsi')
+)
+
+class NewsArticle(models.Model):
+    headlineOrg = models.CharField(max_length=300)
+    headlineEng = models.CharField(max_length=300)
+    date = models.CharField(max_length=30)
+    articleUrl = models.CharField(max_length=300)
+
+class News(models.Model):
+    source = models.CharField(max_length=30)
+    webisteUrl = models.CharField(max_length=300)
+    artilce = models.ManyToManyField(NewsArticle, blank=True)
+    language = models.CharField(max_length=30, choices=language_choices)
--- NewsScrapper/views.py
+++ NewsScrapper/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from .utils import *
 
 # Create your views here.
-
 def homepage(request):
-    return render(request, 'home.html')
-
+    return render(request, 'home/index.html')
